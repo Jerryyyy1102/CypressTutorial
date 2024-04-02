eb5aa52caff99e57b6f2f2b206dd51a2149901e9
David Dubbeldam <khuchongnhunghva@gmail.com>
2024-04-02 09:50:05 +0700

    Upd  variable
    Upd  variable

--- cypress/Practice/practice_b.js
+++ cypress/Practice/practice_b.js
@@ -1,10 +1,11 @@
 let otp="";
+const inpP="0765641761";
 describe('Access to any website, then check content automatically',function () {
     it('Visits the Kitchen Sink',{defaultCommandTimeout: 5000}, function() {
       cy.visit('/')
       cy.get('.mt-2 > span').click()
-      cy.get('#phone').type('0765641761').should('have.value','0765641761').then(p=>{
-        
+      cy.get('#phone').type(inpP).should('have.value','0765641761').then(p=>{
+
         cy.get('button').eq(1).click()
         if(p.length==10)
         {
